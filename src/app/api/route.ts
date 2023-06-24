fe737d6033b584e8124c04a24c1f9258d857bb3f
panditdhamdhere <dhamdherepandit@gmail.com>
2023-06-24 11:44:43 +0530

    backend added

---
+++ src/app/api/route.ts
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+// New func added by Pandit
+
+export async function POST(req: Request, res: NextResponse) {
+    const body = await req.json()
+  
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: body.messages,
+    });
+    console.log(completion.data.choices[0].message);
+    const theResponse = completion.data.choices[0].message;
+  
+    return NextResponse.json({ output: theResponse }, { status: 200 })
+  
+  };
